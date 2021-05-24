3972b802e9ad5aedb2793770cd0fbcb504c5061c
Harry Gustave <harrygustave@harrys-mbp.lan>
2021-05-23 22:36:24 -0500

    created login / sign up page, added script to allow user to sign up. Next push I will have the main gig page llinked to the sign in page where they will be able to see their gigs etc

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector(".right-panel-link");
+
+const signup_box = document.querySelector(".right-panel-link");
+
+right-panel-link.onclick = ()=>{
+  
+    right-panel-link.classList.add("form"); 
+    showQuetions(0); 
+    queCounter(1); 
+    startTimer(15); 
+    startTimerLine(0); 
+  }
+
+  
