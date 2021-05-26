e1fac750dd3ef761dd7f86751778d88301385784
Sam Walters <stwalters314@gmail.com>
2021-05-25 19:59:27 -0500

    began work on user and gig api routes for backend

--- controllers/apis/gig-routes.js
+++ controllers/apis/gig-routes.js
@@ -13,3 +13,29 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.get('/:id', (req, res) => {
+    Gig.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'title', 'description' ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No gig found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
