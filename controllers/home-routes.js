db9d7351772c834b07191f92831a444acf6965b9
Sam Walters <stwalters314@gmail.com>
2021-05-25 20:38:22 -0500

    completed home-routes in controllers

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,1 +1,87 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Gig, User } = require('../models');
 
+
+
+
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+});
+
+
+
+router.get('/', (req, res) => {
+    Gig.findAll({
+      attributes: [
+        'id',
+        'name',
+        'description',
+        'date'
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+    .then(dbGigData => {
+    const gigs = dbGigData.map(gig => gig.get({ plain: true }));
+
+    res.render('homepage', {
+        gigs,
+        loggedIn: req.session.loggedIn
+    });
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
+
+
+router.get('/gig/:id', (req, res) => {
+    Gig.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'name',
+        'description',
+        'date'
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbGigData => {
+        if (!dbGigData) {
+          res.status(404).json({ message: 'No gig found with this id' });
+          return;
+        }
+  
+        const gig = dbGigData.get({ plain: true });
+  
+        res.render('single-gig', {
+          gig,
+          loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  module.exports = router;
