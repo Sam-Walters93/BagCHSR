ececa47efbb96589c06e9559711ce39703d0432e
Michael Rodriguez <mikeyrod22@gmail.com>
2021-05-26 14:03:53 -0500

    get user/users

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const sequelize = require("../../config/connection");
+const { User, Gig } = require("../../models");
+
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: {
+      exclude: ["password"],
+    },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No such user exists" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
