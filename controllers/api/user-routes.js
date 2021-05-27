aac1c535395da7b3b954dd1e3df4b9294a26135f
Michael Rodriguez <mikeyrod22@gmail.com>
2021-05-27 01:11:31 -0500

    update user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -10,7 +10,6 @@ router.get("/", (req, res) => {
   })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
-      console.log(err);
       res.status(500).json(err);
     });
 });
@@ -30,7 +29,101 @@ router.get("/:id", (req, res) => {
       res.json(dbUserData);
     })
     .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  User.create({
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.email = dbUserData.email;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
+    .catch((err) => {
       console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No account associated with that email address." });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "This password is incorrect." });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.email = dbUserData.email;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: "You are now logged in." });
+    });
+  });
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put("/:id", (req, res) => {
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: "No user found with this id." });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id." });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
       res.status(500).json(err);
     });
 });
